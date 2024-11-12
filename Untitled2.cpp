a92f7c0dd3c74c2efe4656bdec0935b94f8c2156
saifan14 <imca2423@rajagiri.edu>
2024-11-12 10:07:08 +0530

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,a[2][2];
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("enter value for %d%d\t",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d\t",a[i][j]);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
